63c71049a66fc8f24030074573e4032789a6347c
nickvirzi <navirzi@gmail.com>
2022-11-30 23:30:45 -0500

    mach-1 COMPLETE
    Setting everything up, getting driver ready to scrape. This is min needed to make work

--- nflData.py
+++ nflData.py
@@ -1,1 +1,8 @@
-yeet = 0
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+#Sets up driver
+DRIVER_PATH = r'C:\ChromeDriver\chromedriver.exe'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://www.espn.com/')
