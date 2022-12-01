dd2ae06a8d255a5990c25223ff28df4231ddcf2d
nickvirzi <navirzi@gmail.com>
2022-11-30 23:54:10 -0500

    mach-2.1 COMPLETE
    Minor formatting adjustments, adding comments, and removing obsolete headers.

--- nflData.py
+++ nflData.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+#Sets up driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get('https://www.espn.com/')
+driver.get('')
