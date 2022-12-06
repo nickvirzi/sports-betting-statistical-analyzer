e1aa05970c4b089c7bdedd485a3cfa67dcf298c6
nickvirzi <navirzi@gmail.com>
2022-12-06 00:05:38 -0500

    master COMMIT
    File test.

--- home.py
+++ home.py
@@ -12,6 +12,6 @@ def index():
     os.system('python vsinDailyTracker.py')
     return 'Hello World'
 
-
 if __name__ == '__main__':
-    app.run(debug=True, port=33507)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
