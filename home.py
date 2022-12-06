165910aa299055a32cdf2cdf4cff450fe9460a3a
nickvirzi <navirzi@gmail.com>
2022-12-06 00:21:00 -0500

    master COMMIT
    File test.

--- home.py
+++ home.py
@@ -13,5 +13,5 @@ def index():
     return 'Hello World'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT'))
+    app.run(host='0.0.0.0', port=port, debug=True)
