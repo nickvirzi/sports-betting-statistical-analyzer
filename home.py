a50fb341b9fdbaa4a71711501b80d02e5c040cfb
nickvirzi <navirzi@gmail.com>
2022-12-06 20:55:19 -0500

    master COMMIT
    FT

--- home.py
+++ home.py
@@ -8,4 +8,5 @@ def index():
     return 'Hello World'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get('PORT'))
+    app.run(host='0.0.0.0', port=port, debug=True)
