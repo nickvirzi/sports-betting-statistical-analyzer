f751f36cb23894177c21d16d0324ba0a318fedac
nickvirzi <navirzi@gmail.com>
2022-12-07 18:18:15 -0500

    master COMMIT
    FT

--- vsinDailyTracker.py
+++ vsinDailyTracker.py
@@ -2,11 +2,11 @@ import pymongo
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from pymongo.server_api import ServerApi
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from pymongo.server_api import ServerApi
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
