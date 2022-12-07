41b851c9214f2328aea2e6b23d6fbca53495c258
nickvirzi <navirzi@gmail.com>
2022-12-07 18:45:53 -0500

    master COMMIT
    FT

--- vsinDailyTracker.py
+++ vsinDailyTracker.py
@@ -16,9 +16,9 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.binary_location = GOOGLE_CHROME_BIN
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-#browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 # #This script is design to capture the vsin data and send it to a database to be tracked and interpreted
 
