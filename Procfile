e2dfb962c57f950f253e3b4b71aa856fe9375dde
nickvirzi <navirzi@gmail.com>
2022-12-06 20:24:38 -0500

    master COMMIT
    FT.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:${PORT} home:app
+web: gunicorn --bind 0.0.0.0:8080 home:app
--- home.py
+++ home.py
@@ -8,5 +8,4 @@ def index():
     return 'Hello World'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT'))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
