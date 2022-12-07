a5863b0a7d43c9aa193344768c11e2c81244bc8c
nickvirzi <navirzi@gmail.com>
2022-12-06 20:08:14 -0500

    master COMMIT
    File test.

--- home.py
+++ home.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 def index():
     return 'Hello World'
 
-#@app.route('/runVSIN')
-#def runVSIN():
-    os.system('python vsinDailyTracker.py')
-    return 'Hello World'
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT'))
     app.run(host='0.0.0.0', port=port, debug=True)
