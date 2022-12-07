8ec525e3109eb293d57ec5bc045f3133705d5ced
nickvirzi <navirzi@gmail.com>
2022-12-07 18:08:43 -0500

    master COMMIT
    FT

--- vsinDailyTracker.py
+++ vsinDailyTracker.py
@@ -15,7 +15,7 @@ CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
+chrome_options.binary_location = GOOGLE_CHROME_PATH 
 
 browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
