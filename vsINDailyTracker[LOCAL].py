3c2208eb2d10f9ce3ed9547aa1eaca25b25ececf
nickvirzi <navirzi@gmail.com>
2022-12-07 22:20:47 -0500

    master COMMIT
    FT

--- vsINDailyTracker[LOCAL].py
+++ vsINDailyTracker[LOCAL].py
@@ -3,59 +3,49 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from pymongo.server_api import ServerApi
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 
-GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
-CHROMEDRIVER_PATH = Service('/app/.chromedriver/bin/chromedriver')
+#This script is design to capture the vsin data and send it to a database to be tracked and interpreted
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.binary_location = GOOGLE_CHROME_BIN
-driver = webdriver.Chrome(service=CHROMEDRIVER_PATH, options=chrome_options)
+#Notes-------------------------------------------------------------------------------------------------
+# - All games upload with current date, needs to be its date, index through table looking for headers maybe?
+# - When clicking a different league header u gotta use full XPATH's bc they use active HTML switching
+# - Automate gathering all leagues
 
-# #This script is design to capture the vsin data and send it to a database to be tracked and interpreted
-
-# #Notes-------------------------------------------------------------------------------------------------
-# # - All games upload with current date, needs to be its date, index through table looking for headers maybe?
-# # - When clicking a different league header u gotta use full XPATH's bc they use active HTML switching
-# # - Automate gathering all leagues
-
-# #Sets up driver
+#Sets up driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://www.vsin.com/betting-resources/daily-betting-insights-for-mlb-nba-nhl/')
+driver.maximize_window()
 
-# #Sets up current time and date formatting
+#Sets up current time and date formatting
 now = datetime.now()
 currentTimeMilitary = now.strftime("%H:%M")
 currentTimeStandard = datetime.strptime(currentTimeMilitary, "%H:%M")
 displayTime = currentTimeStandard.strftime("%r")
 date = now.strftime("%m/%d/%Y")
 
-# #VSIN uses an IFrame which requires a switch to the frame
+#VSIN uses an IFrame which requires a switch to the frame
 WebDriverWait(driver, 5).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="main-content"]/div[2]/div[1]/div/iframe')))
 league = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[1]/div[1]/a[1]/span').text
 
-# #Connects to mongo and sets up collections and databases
+#Connects to mongo and sets up collections and databases
 client = pymongo.MongoClient("mongodb+srv://nickvirzi:sbsa@cluster0.rlqm7dy.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 vsinDailyTrackerDatabase = client["VSINDailyTrackerDatabase"]
 dayLegueTimeCollection = vsinDailyTrackerDatabase[date + ' - ' + league + ' - ' + displayTime]
 
 listOfMatchupData = []
 
-# #Adds raw matchup data for a sport to an array, also checks to stop the table because it will go on forever
+#Adds raw matchup data for a sport to an array, also checks to stop the table because it will go on forever
 for tableRow in driver.find_elements(By.XPATH, '//*[@id="dksplits"]/tbody//tr'):
     matchupDataRow = [item.text for item in tableRow.find_elements(By.XPATH, './/*[self::td]')]
     if matchupDataRow[0] == '' and matchupDataRow[1] == '': break
     listOfMatchupData.append(matchupDataRow)
 
-print(listOfMatchupData)
-
-# #Neatly formats and enters data into the SBSA Database
+#Neatly formats and enters data into the SBSA Database
 for fullMatchupData in listOfMatchupData:
     teamNameData = fullMatchupData[0].split()
     teamSpreadData = fullMatchupData[1].split()
